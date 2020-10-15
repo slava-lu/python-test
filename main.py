3198b3f178ac8de1348e73d6db106e5ffa866417
sashalush8 <71042273+sashalush8@users.noreply.github.com>
2020-10-15 10:39:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ test3([1, 2, 3], [4, 5, 6])
 # есть словарь Примеры ниже. Если в словаре нет поля name, вывести "имя не задано". Если есть имя и sex,
 # вывести 'Lisa is a girl' или 'Slava is a boy, где имя берется из ключа name, а girl или boy определяются ключем sex'
 # Если sex не задан либо не соответсвует этим двум значением, вывесли "Vasya is someone", где имя берется из ключа name
+
 dict1 = {
     'name': 'Slava',
     'sex': 'male'
@@ -57,18 +58,43 @@ dict2 = {
     'sex': 'female'
 }
 dict3 = {
-    'name': 'Vasya',
+    'name': 'Vasiliy',
+    'sex': 'abs'
+}
+dict4 = {
+    'age': '33',
+    'sex': 'abs'
+}
+dict5 = {
+    'name': 'Ivanka',
     'sex': 'abs'
 }
 
 
 def test4(mydict):
     result = ''
-    # код здес
-    print(result)
+    
 
+    def create_text():
+        if "name" not in mydict:
+            return "имя не задано"
+        if "male" in mydict.values():
+            return "{} is a boy".format(mydict["name"])
+        if "female" in mydict.values():
+            return "{} is a girl".format(mydict["name"])
+        if "female" or "male" not in mydict.values():
+            return "{} is someone".format(mydict["name"])
+        
+    result = create_text()
+
+                
+    print(result)
+    
+            
 
 # Задание выше
 test4(dict1)
 test4(dict2)
 test4(dict3)
+test4(dict4)
+test4(dict5)
